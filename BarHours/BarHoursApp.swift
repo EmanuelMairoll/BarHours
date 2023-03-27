bf407191b6b66aa6a46768ffd3f72dd3d710c0c3
Emanuel Mairoll <emanuel.mairoll@authenticvision.com>
2023-03-27 23:20:27 +0200

    Initial Commit

---
+++ BarHours/BarHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarHoursApp.swift
+//  BarHours
+//
+//  Created by Emanuel Mairoll on 27.03.23.
+//
+
+import SwiftUI
+
+@main
+struct BarHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
